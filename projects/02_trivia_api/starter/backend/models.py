5ff18f260760f4ee699081f57b168da9d81fdc99
Reema <reema.sh2211@gmail.com>
2021-07-15 16:42:04 +0300

    fixing the user name

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
-DB_USER = os.getenv('DB_USER', 'postgres')  
+DB_USER = os.getenv('DB_USER', 'reemaalhammadi')  
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
 DB_NAME = os.getenv('DB_NAME', 'trivia')  
 DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
