2b7d89df406d4278689cb6dde86c43b9e0617b29
Reema <reema.sh2211@gmail.com>
2021-09-07 10:35:30 +0300

    test

--- projects/capstone/models.py
+++ projects/capstone/models.py
@@ -7,7 +7,7 @@ import json
 DB_PATH = os.environ['DATABASE_URI']
 db = SQLAlchemy()
 
-
+# DATABASE_URL: postgres://oyjsadyvqkreqn:1c11411b0cfe70a8d0426bbb548f150f15f7488a0f76dd66778e33809b859801@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d6843dvh1h4uhs
 def setup_db(app, database_path=DB_PATH):
     app.config['SQLALCHEMY_DATABASE_URI'] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
--- projects/capstone/setup.sh
+++ projects/capstone/setup.sh
@@ -1,4 +1,4 @@
 
-export DATABASE_URI='postgres://root:****@localhost:5432/finalproject'
+export DATABASE_URI='postgres://oyjsadyvqkreqn:1c11411b0cfe70a8d0426bbb548f150f15f7488a0f76dd66778e33809b859801@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d6843dvh1h4uhs'
 export FLASK_APP=app.py
 export FLASK_ENV=development
